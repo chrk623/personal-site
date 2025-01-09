a3fb2a78aab1d6995d7eec95824e661eac17eea3
Charco Hui <charcohui93@gmail.com>
2025-01-09 13:50:20 +1300

    fix: contentlayer config build

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -8,11 +8,11 @@ import remarkGfm from "remark-gfm";
 const computedFields = {
   slug: {
     type: "string",
-    resolve: (doc) => `/${doc._raw.flattenedPath}`,
+    resolve: (doc: any) => `/${doc._raw.flattenedPath}`,
   },
   slugAsParams: {
     type: "string",
-    resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/"),
+    resolve: (doc: any) => doc._raw.flattenedPath.split("/").slice(1).join("/"),
   },
 };
 
@@ -40,6 +40,7 @@ export const Post = defineDocumentType(() => ({
       type: "string",
     },
   },
+  // @ts-ignore
   computedFields,
 }));
 
@@ -48,38 +49,23 @@ export default makeSource({
   documentTypes: [Post],
   mdx: {
     remarkPlugins: [remarkGfm],
-    // remarkPlugins: [[remarkGfm, { singleTilde: false }]],
-    // remarkPlugins: [
-    //   [
-    //     remarkGfm,
-    //     {
-    //       singleTilde: false,
-    //       tablePipeAlign: false,
-    //       tableCellPadding: false,
-    //     },
-    //   ],
-    // ],
     rehypePlugins: [
       rehypeSlug,
       [
-        rehypePrettyCode,
+        rehypePrettyCode as any,
         {
           theme: "catppuccin-mocha",
-          // theme: {
-          //   light: "catppuccin-latte", // Light theme
-          //   dark: "catppuccin-mocha", // Dark theme
-          // },
-          onVisitLine(node) {
+          onVisitLine(node: any) {
             // Prevent lines from collapsing in `display: grid` mode, and allow empty
             // lines to be copy/pasted
             if (node.children.length === 0) {
               node.children = [{ type: "text", value: " " }];
             }
           },
-          onVisitHighlightedLine(node) {
+          onVisitHighlightedLine(node: any) {
             node.properties.className.push("line--highlighted");
           },
-          onVisitHighlightedWord(node) {
+          onVisitHighlightedWord(node: any) {
             node.properties.className = ["word--highlighted"];
           },
         },
